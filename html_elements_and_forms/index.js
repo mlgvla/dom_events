dfb5147743cc74a68eee236bd2d6bf772178f97e
Monica Gerard <mlgvla@gmail.com>
2023-04-08 11:29:01 -0400

    fixes titles

--- html_elements_and_forms/index.js
+++ html_elements_and_forms/index.js
@@ -1,34 +1,36 @@
+const radioButtons = document.getElementsByName("radioButtons")
+const checkboxes = document.getElementsByName("checkbox")
+const dropdown = document.getElementById("dropdown")
 
-         const radioButtons = document.getElementsByName("radioButtons")
-         const checkboxes = document.getElementsByName("checkbox")
-         const dropdown = document.getElementsByName("dropdown")[0]
+radioButtons.forEach((radioButton) => {
+   radioButton.addEventListener("change", function (e) {
+      console.log(e)
+      console.log("Event Type: ", e.type)
+      console.log("Selected radio button:", e.target.value)
+   })
+})
 
-         for (let i = 0; i < radioButtons.length; i++) {
-            radioButtons[i].addEventListener("change", function (e) {
-               console.log(e)
-               console.log("Event Type: ", e.type)
-               console.log("Selected radio button:", e.target.value)
-            })
-         }
+checkboxes.forEach((checkbox) => {
+   checkbox.addEventListener("change", function (e) {
+      console.log(e)
+      console.log("Event Type: ", e.type)
+      console.log(`${e.target.value} is checked:`, e.target.checked)
+   })
+})
 
-         for (let i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].addEventListener("change", function (e) {
-               console.log(e)
-               console.log("Event Type: ", e.type)
-               console.log(`${e.target.value} is checked:`, e.target.checked)
-            })
-         }
+// bubbling!
+// const checkboxFS = document.querySelector("#checkbox_group")
 
-         // bubbling!
-         const checkboxFS = document.querySelector("#checkbox-group")
-         checkboxFS.addEventListener("change", (e) => {
-            console.log(e)
-            console.log("Event Type: ", e.type)
-            console.log(`${e.target.value} is checked:`, e.target.checked)
-         })
+// checkboxFS.addEventListener("change", (e) => {
+//    console.log("Current Target: ", e.currentTarget)
+//    console.log(e)
+//    console.log("Event Type: ", e.type)
+//    console.log(`${e.target.value} is checked:`, e.target.checked)
+// })
 
-         dropdown.addEventListener("change", function (e) {
-            console.log(e)
-            console.log("Event Type: ", e.type)
-            console.log("Selected option in dropdown:", e.target.value)
-         })
+dropdown.addEventListener("change", function (e) {
+   console.log(e.currentTarget)
+   console.log(e)
+   console.log("Event Type: ", e.type)
+   console.log("Selected option in dropdown:", e.target.value)
+})
