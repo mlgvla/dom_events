648d884889b164e0f3641c217ed7423798fae657
Monica Gerard <mlgvla@gmail.com>
2023-04-09 00:21:18 -0400

    comments out focus/blur code

--- 01-dom_event_intro/index.js
+++ 01-dom_event_intro/index.js
@@ -2,12 +2,17 @@ const btn = document.querySelector("button") // get element by type
 const div = document.querySelector("div") 
 
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", changeBackground )
+
+
+function changeBackground(event) {
+   console.log(event)
    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})` // generate a random color
 
    div.style.backgroundColor = rndCol // change the background color of the div
 }
-)
+
+// btn.removeEventListener("click", changeBackground)
 
 function random(number) {
    return Math.floor(Math.random() * (number + 1)) // generate a random number from 0 to 255
--- 03-keyboard_events/index.js
+++ 03-keyboard_events/index.js
@@ -8,29 +8,11 @@ const textBox = document.querySelector("#text-box")
 const output = document.querySelector("#output")
 const playPianoBtn = document.querySelector("#play-piano")
 
-
 // Keydown demo
 
 textBox.addEventListener("keydown", (e) => {
    console.log(e)
-   output.textContent = `You pressed the "${e.key} key".`
-})
-
-// Focus/blur demo
-
-textBox.addEventListener("focus", (e) => {
-   console.log(e)
-   document.removeEventListener("keydown", toyPianoPlay)
-   e.target.style.backgroundColor = "blue"
-   e.target.style.color = "white"
-})
-
-textBox.addEventListener("blur", (e) => {
-   console.log(e)
-   e.target.style.backgroundColor = ""
-   e.target.style.color = ""
-   e.target.value = ""
-   output.textContent = ""
+   output.textContent = `You pressed "${e.key}".`
 })
 
 // Piano demo - using key event properties
@@ -40,26 +22,47 @@ playPianoBtn.addEventListener("click", (e) => {
 })
 
 function toyPianoPlay(e) {
-   if (e.keyCode === 74) {    // the "j" key
+   if (e.keyCode === 74) {
+      // the "j" key
       g3.currentTime = 0
       g3.play()
    }
-   if (e.keyCode === 75) {    // the "k" key
+   if (e.keyCode === 75) {
+      // the "k" key
       a3.currentTime = 0
       a3.play()
    }
-   if (e.keyCode === 76) {    // the "l" key
+   if (e.keyCode === 76) {
+      // the "l" key
       b3.currentTime = 0
       b3.play()
    }
-   if (false) {   // hook this up to the ; key!
+   if (e.keyCode === 186) {
+      // hook this up to the ; key!
       c4.currentTime = 0
       c4.play()
    }
 
-   if (false) {   // hook this up to the ' key!
+   if (e.keyCode === 222) {
+      // hook this up to the ' key!
       d4.currentTime = 0
       d4.play()
    }
 }
 
+// Focus/blur demo
+
+textBox.addEventListener("focus", (e) => {
+   // console.log(e)
+   document.removeEventListener("keydown", toyPianoPlay)
+   // e.target.style.backgroundColor = "blue"
+   // e.target.style.color = "white"
+})
+
+textBox.addEventListener("blur", (e) => {
+   // console.log(e)
+   e.target.style.backgroundColor = ""
+   e.target.style.color = ""
+   e.target.value = ""
+   output.textContent = ""
+})
